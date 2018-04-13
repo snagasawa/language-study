c1492fc8e51440a610dd41b587f26fa74bb32f04
snagasawa <whitecrane8920@gmail.com>
2018-04-13 15:37:49 +0900

    レイアウト調整

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <router-view/>
+    <el-container style="height: 100%; border: 1px solid #eee">
+      <el-header style="text-align: right; font-size: 12px">
+      </el-header>
+      <el-container>
+        <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+          <nav-menu></nav-menu>
+        </el-aside>
+        <router-view/>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import NavMenu from "@/components/NavMenu";
+
 export default {
-  name: 'App',
+  name: "App",
+  components: {
+    "nav-menu": NavMenu
+  }
 };
 </script>
 
--- src/components/TextReader.vue
+++ src/components/TextReader.vue
@@ -1,25 +1,19 @@
 <template>
-  <div>
-    <el-container style="height: 100%; border: 1px solid #eee">
-      <el-container>
-        <el-header style="text-align: right; font-size: 12px">
-        </el-header>
-        <el-main>
-          <el-input
-            style="200px"
-            type="textarea"
-            :autosize="{ minRows: 10 }"
-            placeholder="Please input"
-            v-model="input">
-          </el-input>
-          <div style="padding: 10px;" />
-          <el-row>
-            <el-button type="primary" v-on:click="read">読み上げ</el-button>
-          </el-row>
-        </el-main>
-      </el-container>
-    </el-container>
-  </div>
+  <el-container>
+    <el-main>
+      <el-input
+        style="200px"
+        type="textarea"
+        :autosize="{ minRows: 10 }"
+        placeholder="Please input"
+        v-model="input">
+      </el-input>
+      <div style="padding: 10px;" />
+      <el-row>
+        <el-button type="primary" v-on:click="read">読み上げ</el-button>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <style>
