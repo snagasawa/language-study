3dbd933bfe5357b7eea7c935e59fbf6e831aff9f
snagasawa <whitecrane8920@gmail.com>
2018-04-13 15:37:29 +0900

    NavMenu

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-menu>
+    <el-submenu index="1">
+    <template slot="title"><i class="el-icon-menu"></i>メニュー</template>
+      <router-link to="/text_reader">
+        <el-menu-item index="1-1">音声読み上げ</el-menu-item>
+      </router-link>
+      <router-link to="/">
+        <el-menu-item index="1-2">音声入力</el-menu-item>
+      </router-link>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'NavMenu'
+}
+</script>
